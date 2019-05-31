acaf506b2744d24c9f6908fb469860953d6c9939
marbaez1714 <marbaez1714@gmail.com>
2019-05-30 21:00:16 -0500

    health bars!

--- src/components/Examples/ProgressExamples.js
+++ src/components/Examples/ProgressExamples.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Progress from "../Progress";
+
 export default function ProgressExamples() {
+  const [primaryProgress, setPrimaryProgress] = useState(1);
+  const [secondaryProgress, setSecondaryProgress] = useState(1);
+  const [successProgress, setSuccessProgress] = useState(1);
+  const [warningProgress, setWarningProgress] = useState(1);
+  const [errorProgress, setErrorProgress] = useState(1);
+
+  useEffect(() => {
+    setInterval(() => {
+      const rand1 = Math.random() * 10;
+      const rand2 = Math.random() * 10;
+      const rand3 = Math.random() * 10;
+      const rand4 = Math.random() * 10;
+      const rand5 = Math.random() * 10;
+      setPrimaryProgress(rand1);
+      setSecondaryProgress(rand2);
+      setSuccessProgress(rand3);
+      setWarningProgress(rand4);
+      setErrorProgress(rand5);
+    }, 5000);
+  }, []);
+
   return (
     <div className="example">
       <div>
         <h3 style={{ textAlign: "left" }}>Progress!</h3>
       </div>
-      <Progress type="primary" total={100} current={10} />
-      <Progress type="secondary" total={100} current={20} />
-      <Progress type="success" total={100} current={30} />
-      <Progress type="warning" total={100} current={40} />
-      <Progress type="error" total={100} current={60} />
+      <Progress type="primary" total={100} current={primaryProgress * 10} />
+      <Progress tick type="primary" total={10} current={primaryProgress} />
+      <Progress type="secondary" total={100} current={secondaryProgress * 10} />
+      <Progress tick type="secondary" total={10} current={secondaryProgress} />
+      <Progress type="success" total={100} current={successProgress * 10} />
+      <Progress tick type="success" total={10} current={successProgress} />
+      <Progress type="warning" total={100} current={warningProgress * 10} />
+      <Progress tick type="warning" total={10} current={warningProgress} />
+      <Progress type="error" total={100} current={errorProgress * 10} />
+      <Progress tick type="error" total={10} current={errorProgress} />
     </div>
   );
 }
--- src/components/Progress.js
+++ src/components/Progress.js
@@ -2,8 +2,25 @@ import React from "react";
 import "./Progress.scss";
 
 export default function Progress(props) {
-  const { total, current, type = "primary" } = props;
+  const { total, current, tick, type = "primary" } = props;
   const progress = `${(current / total) * 100}%`;
+
+  if (tick) {
+    const tickMarks = [];
+    for (let i = 1; i <= total; i++) {
+      tickMarks.push(
+        <div
+          className={`progress-tick ${i === parseInt(current) && "currentTick"} ${i <= current &&
+            type}`}
+        />
+      );
+    }
+    return (
+      <div className="progress-bar">
+        <div className="tick-bar-inner">{tickMarks}</div>
+      </div>
+    );
+  }
 
   return (
     <div className="progress-bar">
